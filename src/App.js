43870d6a900491600610759d912961b1046fa60f
Jaime Neves <jaimeneves@MacBook-Jaimeneeves.local>
2021-04-03 19:44:57 -0300

    Adição do React Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <main className="container">
-      <div className="row">
-        <div className="col">
-          <h2 className="mt-5">React Projeto GitHub</h2>
-          <form id="gitHubForm" className="form-inline m-auto">
-            <input id="usernameInput" className="form-control mb-5" type="text" name="username" placeholder="GitHub Username" />
-            <input type="submit" className="btn btn-primary ml-2 mb-5" value="Submit" />
-          </form>
-            <div className="main-content"></div>
+    <Router>
+      <main className="container">
+        <div className="row">
+          <div className="col">
+            <h2 className="mt-5">React Projeto GitHub</h2>
+            <form id="gitHubForm" className="form-inline m-auto">
+              <input id="usernameInput" className="form-control mb-5" type="text" name="username" placeholder="GitHub Username" />
+              <input type="submit" className="btn btn-primary ml-2 mb-5" value="Submit" />
+            </form>
+              <div className="main-content"></div>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </Router>
   );
 }
 
