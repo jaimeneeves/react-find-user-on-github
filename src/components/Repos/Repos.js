3276d722c124649b454158c837b12aebd2e04442
Jaime Neves <jaimeneves@MacBook-Jaimeneeves.local>
2021-04-04 13:42:42 -0300

    Criação do compoonente Repos e listagem dos repositórios do usuário

---
+++ src/components/Repos/Repos.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class Repos extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      repos: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://api.github.com/users/${this.props.match.params.username}/repos?access_token=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`)
+    .then(response => response.json())
+    .then(repos => {
+        // How can we use `this` inside a callback without binding it??
+        // Make sure you understand this fundamental difference with arrow functions!!!
+        this.setState({
+          repos: repos
+        });
+      });
+  }
+
+  render() {
+
+    if (!this.state.repos) {
+      return (<div className="followers-page">LOADING...</div>);
+    }
+
+    return (
+      <div className="followers-page mb-5">
+        <h3>{this.props.match.params.username} repositórios</h3>
+        <ul className="list-group">
+        {
+          this.state.repos.map(function(repo) {
+            return <li key={repo.id} className="list-group-item d-flex justify-content-between align-items-center">
+              <a rel="noreferrer" href={repo.html_url} target="_blank">{repo.full_name}</a>
+              <span className="badge badge-primary badge-pill text-right">{repo.stargazers_count}★</span>
+            </li>;
+          })
+        }
+        </ul>
+      </div>
+    );
+  }
+}
+
+Repos.propTypes = {};
+
+Repos.defaultProps = {};
+
+export default Repos;
